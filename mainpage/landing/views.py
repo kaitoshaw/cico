7b357a780cd0cf39d638266dff94f2b53fffb933
kaitoshaw <jeconiahri@gmail.com>
2023-12-11 22:51:06 +0700

    Added login and register functions to return each HTML

--- mainpage/landing/views.py
+++ mainpage/landing/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'landing/index.html')
+
+def login(request):
+    return render(request, 'landing/login.html')
+
+def register(request):
+    return render(request, 'landing/register.html')
