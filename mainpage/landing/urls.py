2b80787c529d880d08eb9c2cf9e8457a24dc3400
kaitoshaw <jeconiahri@gmail.com>
2023-12-11 22:50:04 +0700

    Added login page and regsiter page to URLS.py

--- mainpage/landing/urls.py
+++ mainpage/landing/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'), # Landing Page
+    path('login', views.login, name='login'), # Login page
+    path('register', views.register, name='register') # Register Page
 ]
